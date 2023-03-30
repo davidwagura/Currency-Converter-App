d1583816b6417f2b6d0384fffb880bd3bd106e7b
davidwagura <davidwagura83@gmail.com>
2023-03-30 20:55:35 +0300

    creat the front end

---
+++ index.js
@@ -0,0 +1,18 @@
+new Vue ({
+  el: '#app',
+  data: {
+    KSH: '500',
+    USD: '125',
+    UG: '300',
+    TZ: '200'
+
+  },
+
+  computed: {
+    convert() {
+      return
+    }
+  },
+
+  methods: {}
+})
