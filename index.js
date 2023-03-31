637e8054542442ca13d431dfaf34944e2822e3c2
davidwagura <davidwagura83@gmail.com>
2023-03-31 16:09:58 +0300

    add vue.js

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
 new Vue ({
   el: '#app',
+
   data: {
-    KSH: '500',
-    USD: '125',
-    UG: '300',
-    TZ: '200'
-
+    amountToConvert: 0,
+    currencyFrom: '',
+    currencyTo: '',
+    result: 0,
+    showResults: false
   },
 
-  computed: {
+  computed: {},
+
+  methods: {
     convert() {
-      return
-    }
-  },
+      if(this.currencyFrom === 'USD') {
+        if(this.currencyTo === 'KSH') {
+              this.result = this.amountToConvert * 132.27;
+        }
+        if(this.currencyTo === 'UG'){
+            this.result = this.amountToConvert * 3768;
+        }
+        if(this.currencyTo === 'TZ'){
+            this.result = this.amountToConvert * 2339;
+        }
+      }
+      else if(this.currencyFrom === 'KSH') {
+          if(this.currencyTo === 'USD'){
+            this.result = this.amountToConvert * 0.00076;
+        }
+          if(this.currencyTo === 'UG'){
+            this.result = this.amountToConvert * 29;
+        }
+          if(this.currencyTo === 'TZ'){
+            this.result = this.amountToConvert * 18;
+        }
+      }
+      else if(this.currencyFrom === 'TZ') {
+          if(currencyTo === 'KSH'){
+              this.result = this.amountToConvert * 0.057;
+        }
+          if(currencyTo === 'UG'){
+            this.result = this.amountToConvert * 2;
+        }
+          if(currencyTo === 'USD'){
+            this.result = this.amountToConvert * 0.00043;
+        }
+      }
+      else if(this.currencyFrom === 'UG') {
+          if(currencyTo === 'KSH'){
+              this.result = this.amountToConvert * 0.035;
+        }
+          if(currencyTo === 'TZ'){
+            this.result = this.amountToConvert * 0.6;
+        }
+          if(currencyTo === 'USD'){
+            this.result = this.amountToConvert * 0.00026;
+        }
+      }
 
-  methods: {}
+      this.showResults = true;
+    }
+  }
 })
