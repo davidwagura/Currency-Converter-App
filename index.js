04c9efadc0cd0bd89fb3d83a498370a238b2cb5f
davidwagura <davidwagura83@gmail.com>
2023-03-31 23:27:17 +0300

    add a property to round off the data

--- index.js
+++ index.js
@@ -15,46 +15,46 @@ new Vue ({
     convert() {
       if(this.currencyFrom === 'USD') {
         if(this.currencyTo === 'KSH') {
-              this.result = this.amountToConvert * 132.27;
+              this.result =  Math.ceil(this.amountToConvert * 132.27);
         }
         if(this.currencyTo === 'UG'){
-            this.result = this.amountToConvert * 3768;
+            this.result =  Math.ceil(this.amountToConvert * 3768);
         }
         if(this.currencyTo === 'TZ'){
-            this.result = this.amountToConvert * 2339;
+            this.result =  Math.ceil(this.amountToConvert * 2339);
         }
       }
       else if(this.currencyFrom === 'KSH') {
           if(this.currencyTo === 'USD'){
-            this.result = this.amountToConvert * 0.00076;
+            this.result =  Math.ceil(this.amountToConvert * 0.00076);
         }
           if(this.currencyTo === 'UG'){
-            this.result = this.amountToConvert * 29;
+            this.result =  Math.ceil(this.amountToConvert * 29);
         }
           if(this.currencyTo === 'TZ'){
-            this.result = this.amountToConvert * 18;
+            this.result =  Math.ceil(this.amountToConvert * 18);
         }
       }
       else if(this.currencyFrom === 'TZ') {
           if(this.currencyTo === 'KSH'){
-              this.result = this.amountToConvert * 0.057;
+              this.result =  Math.ceil(this.amountToConvert * 0.057);
         }
           if(this.currencyTo === 'UG'){
-            this.result = this.amountToConvert * 2;
+            this.result = Math.ceil(this.amountToConvert * 2);
         }
           if(this.currencyTo === 'USD'){
-            this.result = this.amountToConvert * 0.00043;
+            this.result = Math.ceil(this.amountToConvert * 0.00043);
         }
       }
       else if(this.currencyFrom === 'UG') {
           if(this.currencyTo === 'KSH'){
-              this.result = this.amountToConvert * 0.035;
+              this.result = Math.ceil(this.amountToConvert * 0.035);
         }
           if(this.currencyTo === 'TZ'){
-            this.result = this.amountToConvert * 0.6;
+            this.result = Math.ceil(this.amountToConvert * 0.6);
         }
           if(this.currencyTo === 'USD'){
-            this.result = this.amountToConvert * 0.00026;
+            this.result = Math.ceil(this.amountToConvert * 0.00026);
         }
       }
 
